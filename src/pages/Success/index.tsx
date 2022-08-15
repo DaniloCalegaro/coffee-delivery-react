7b680e38729c4819da1949aae6e5267f156ed49c
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-08-15 10:55:10 -0300

    feat: layout pagina sucess concluído

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,9 +1,58 @@
-import { SuccessContainer } from './styles'
+import { Clock, CurrencyDollar, MapPin } from 'phosphor-react'
+import {
+  AddressDelivery,
+  DeliveryTime,
+  FormOfPayment,
+  SuccessContainer
+} from './styles'
+
+import ImgDelivery from '../../assets/images/delivery.svg'
 
 export function Success() {
   return (
     <SuccessContainer>
-      <h1>Success</h1>
+      <aside>
+        <h1>Uhu! Pedido confirmado</h1>
+        <span>Agora é só aguardar que logo o café chegará até você</span>
+
+        <div>
+          <AddressDelivery>
+            <i>
+              <MapPin size={20} />
+            </i>
+            <div>
+              <span>
+                Entrega em{' '}
+                <strong>
+                  Rua João Martinelli, 102 <br />
+                </strong>
+                Farrapos - Porto Alegre, RS
+              </span>
+            </div>
+          </AddressDelivery>
+          <DeliveryTime>
+            <i>
+              <Clock size={20} />
+            </i>
+            <div>
+              <span>Previsão de entrega</span>
+              <strong>20 min - 30 min</strong>
+            </div>
+          </DeliveryTime>
+
+          <FormOfPayment>
+            <i>
+              <CurrencyDollar size={20} />
+            </i>
+            <div>
+              <span>Pagamento na entrega</span>
+              <strong>Cartão de Crédito</strong>
+            </div>
+          </FormOfPayment>
+        </div>
+      </aside>
+
+      <img src={ImgDelivery} alt="" />
     </SuccessContainer>
   )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,99 @@
 import styled from 'styled-components'
 
-export const SuccessContainer = styled.main``
+export const SuccessContainer = styled.main`
+  margin-top: 2.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  flex-wrap: wrap;
+
+  img {
+    width: 20rem;
+    margin-top: 1rem;
+
+    @media (min-width: 720px) {
+      width: 30rem;
+    }
+  }
+
+  aside {
+    h1 {
+      font-size: 2rem;
+      color: ${props => props.theme['yellow-dark']};
+      line-height: 140%;
+    }
+
+    > span {
+      font-size: 1.25rem;
+      color: ${props => props.theme['base-subtitle']};
+      display: inline-block;
+      margin-bottom: 2.5rem;
+    }
+
+    > div {
+      background: linear-gradient(
+            ${props => props.theme['background']},
+            ${props => props.theme['background']}
+          )
+          padding-box,
+        linear-gradient(
+            to right,
+            ${props => props.theme['yellow']},
+            ${props => props.theme['purple']}
+          )
+          border-box;
+
+      border: 1px solid transparent;
+      border-radius: 6px 36px;
+      padding: 2.5rem;
+
+      i {
+        display: flex;
+        width: 2rem;
+        height: 2rem;
+        justify-content: center;
+        align-items: center;
+        border-radius: 16px;
+        color: ${props => props.theme['white']};
+        margin-right: 0.75rem;
+      }
+    }
+  }
+`
+
+export const AddressDelivery = styled.div`
+  display: flex;
+  margin-bottom: 1.5rem;
+
+  i {
+    background: ${props => props.theme['purple']};
+  }
+`
+
+export const DeliveryTime = styled.div`
+  display: flex;
+  margin-bottom: 1.5rem;
+
+  i {
+    background: ${props => props.theme['yellow']};
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export const FormOfPayment = styled.div`
+  display: flex;
+
+  i {
+    background: ${props => props.theme['yellow-dark']};
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`
